c4b2fe247a4716d1d38381f94a4a80b67b9e2470
Kabuto <rohmatsetyawan74@gmail.com>
2025-06-17 16:36:25 +0700

    Add files via upload

--- TokoBengkel.cpp
+++ TokoBengkel.cpp
@@ -1,113 +1,123 @@
 #include <iostream>
 #include <string>
+#include <conio.h>
 using namespace std;
+
+void salamPembuka()
+{
+    cout << "Selamat Datang di Bengkel Andalan Anda\n\n";
+}
+
+void pilihanAplikasi()
+{
+	int main();
+	char pilihan;
+    cout << "Apakah Anda Ingin Kembali ke Halaman Utama? \n\n";
+    cout << "Jika Iya Tekan Y "<<endl;
+    cout<<"Jika Tidak Tekan N"<<endl;
+    cout<<"Masukan Pilihan Anda: ";
+    cin>>pilihan;
+    cout<<"==========Tekan Enter untuk Konfirmasi==========";
+    
+    if (pilihan=='y'){
+    	
+    	getch();
+    	system ("cls");
+    	main();
+	}
+	else{
+		cout<<"Terima Kasih, Permintaan Anda Sedang di Proses"<<endl;
+	}
+}
 
 int main()
 {
-    string namaPelanggan;
-    string platNomor;
-    string jenisKendaraan;
-    int hargaServis;
-    string namaServis;
+    string namaPelanggan, platNomor, jenisKendaraan;
+    string namaServis[7] = {"Ganti oli", "Servis Ringan", "Servis Berat", "Ganti Kampas Rem", "Tune Up", "Ganti Lampu", "Cek Crank Case"};
+    int hargaServisMotor[7] = {75000, 500000, 750000, 45000, 300000, 15000, 40000};
+    int hargaServisMobil[7] = {85000, 600000, 800000, 50000, 400000, 20000, 45000};
     char kodeKendaraan;
-    int jenisServis;
+    int jumlahServis, totalHarga = 0;
+    int pilihan[10]; // max 10 layanan
 
     cout << "+_+_+_+_+_+_Bengkel Jaya Terpadu_+_+_+_+_+_+\n\n";
 
-    cout << "Masukan Nama Anda= ";
-    getline(cin, namaPelanggan);
-    cout << "Masukan Plat Nomor Kendaraan= ";
-    getline(cin, platNomor);
-    cout << "Masukan Jenis Kendaraan Anda (motor (A) / mobil (B))= ";
-    getline(cin, jenisKendaraan);
-    cout << endl;
+    salamPembuka();
 
-    cout << "+_+_+_+_+_+_Daftar Harga Sevis_+_+_+_+_+_+\n\n";
+    cout << "Masukan Nama Anda: ";
+    getline(cin, namaPelanggan);
+
+    cout << "Masukan Plat Nomor Kendaraan: ";
+    getline(cin, platNomor);
+
+    cout << "Masukan Jenis Kendaraan Anda (motor / mobil): ";
+    getline(cin, jenisKendaraan);
+
+    cout << "\n+_+_+_+_+_+_Daftar Harga Servis_+_+_+_+_+_+\n\n";
 
     if (jenisKendaraan == "motor")
     {
         kodeKendaraan = 'A';
-        cout << "1. Ganti oli (75.000) " << endl;
-        cout << "2. Servis Ringan (500.000) " << endl;
-        cout << "3. Servis Berat (750.000)" << endl;
-        cout << "4. Ganti Kampas Rem (45.000)" << endl;
-        cout << "5. Tune Up (300.000)" << endl;
+        for (int i = 0; i <7; i++)
+        {
+            cout << i + 1 << ". " << namaServis[i] << " (" << hargaServisMotor[i] << ")\n";
+        }
     }
-
     else if (jenisKendaraan == "mobil")
     {
         kodeKendaraan = 'B';
-        cout << "1. Ganti oli (85.000) " << endl;
-        cout << "2. Servis Ringan (600.000) " << endl;
-        cout << "3. Servis Berat (800.000)" << endl;
-        cout << "4. Ganti Kampas Rem (50.000)" << endl;
-        cout << "5. Tune Up (400.000)" << endl;
-    }
-
-    else
-    {
-        cout << "Jenis kendaraan tidak dikenali";
-    }
-
-    cout << "Masukan Nomor Servis Sesuai Menu : ";
-    cin >> jenisServis;
-
-    if (kodeKendaraan == 'A')
-    {
-        switch (jenisServis)
+        for (int i = 0; i <7; i++)
         {
-        case 1:
-            hargaServis = 75000;
-            namaServis = "Ganti Oli";
-            break;
-        case 2:
-            hargaServis = 500000;
-            namaServis = "Servis ringan";
-            break;
-        case 3:
-            hargaServis = 750000;
-            namaServis = "Servis Berat";
-            break;
-        case 4:
-            hargaServis = 45000;
-            namaServis = "Ganti Kampas Rem";
-            break;
-        case 5:
-            hargaServis = 300000;
-            namaServis = "Tune Up";
-            break;
+            cout << i + 1 << ". " << namaServis[i] << " (" << hargaServisMobil[i] << ")\n";
         }
     }
     else
     {
-        switch (jenisServis)
+        cout << "Jenis kendaraan tidak dikenali.\n";
+        return 0;
+    }
+
+    cout << "\nMasukan jumlah layanan yang ingin dipilih: ";
+    cin >> jumlahServis;
+
+    cout << "Pilih " << jumlahServis << " layanan (masukkan nomor sesuai menu 1-5):\n";
+
+    for (int i = 0; i < jumlahServis; i++)
+    {
+        cout << "Pilihan ke-" << i + 1 << ": ";
+        cin >> pilihan[i];
+
+        if (pilihan[i] < 1 || pilihan[i] > 7)
         {
-        case 1:
-            hargaServis = 85000;
-            namaServis = "Ganti Oli";
-            break;
-        case 2:
-            hargaServis = 600000;
-            namaServis = "Servis ringan";
-            break;
-        case 3:
-            hargaServis = 800000;
-            namaServis = "Servis Berat";
-            break;
-        case 4:
-            hargaServis = 50000;
-            namaServis = "Ganti Kampas Rem";
-            break;
-        case 5:
-            hargaServis = 400000;
-            namaServis = "Tune Up";
-            break;
+            cout << "Pilihan tidak valid!\n";
+            return 0;
+        }
+
+        if (kodeKendaraan == 'A')
+        {
+            totalHarga += hargaServisMotor[pilihan[i] - 1];
+        }
+        else
+        {
+            totalHarga += hargaServisMobil[pilihan[i] - 1];
         }
     }
 
-    cout << "+_+_+_+_+_+_NOTA BELANJA_+_+_+_+_+_+\n\n";
+    cout << "\n+_+_+_+_+_+_NOTA BELANJA_+_+_+_+_+_+\n\n";
     cout << "Nama Pelanggan: " << namaPelanggan << endl;
     cout << "Jenis Kendaraan & Plat Nomor: " << jenisKendaraan << " , " << platNomor << endl;
-    cout << "Jenis Servis: " << namaServis << endl;
-    cout << "Harga Layanan: " << hargaServis << endl;
+    cout << "Layanan yang dipilih:\n";
+
+    for (int i = 0; i < jumlahServis; i++)
+    {
+        int idx = pilihan[i] - 1;
+        cout << "- " << namaServis[idx] << endl;
+    }
+
+    cout << "Total Harga: Rp " << totalHarga << endl<<endl;
+    
+    cout<<"===================================\n\n";
+    
+    pilihanAplikasi();
+    
 }
